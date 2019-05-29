e1b8860270fa99580adbe17228d2e5195c16e67d
Andrew James <ajames20@gmail.com>
2019-05-29 07:06:45 -0500

    Add App updates

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://docker.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Docker
         </a>
       </header>
     </div>
